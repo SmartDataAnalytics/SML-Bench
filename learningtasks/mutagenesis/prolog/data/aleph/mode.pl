36b390471cff903a301529ee80d157c942510488
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2016-04-04 19:04:19 +0200

    Mutagenesis learning task clean-up for Aleph

--- learningtasks/mutagenesis/prolog/data/aleph/mode.pl
+++ learningtasks/mutagenesis/prolog/data/aleph/mode.pl
@@ -35,7 +35,6 @@
 :- modeb(1,member(+ring,+ringlist)).
 :- modeb(1,connected(+ring,+ring)).
 
-
 :- determination(active/1,atm/5).
 :- determination(active/1,bond/4).
 :- determination(active/1,gteq/2).
@@ -60,86 +59,3 @@
 :- determination(active/1,member/2).
 :- determination(active/1,connected/2).
 
-
-
-
-% type information
-
-drug(D):-
-	name(D,[_|X]), name(Num,X), int(Num),
-	Num >= 1, Num =< 230, !.
-
-atomid(A):-
-	name(A,[_|X]),
-	append(Z,[95|Y],X),
-	name(N1,Y),
-	name(N2,Z),
-	int(N1), int(N2),
-	N2 >= 1, N2 =< 230,
-	N1 =< 500, !.
-
-append([],A,A).
-append([H|T],A,[H|T1]):-
-        append(T,A,T1).
-
-
-
-charge(X):-
-        float(X).
-energy(X):-
-        float(X).
-hydrophob(X):-
-        float(X).
-
-% ring([_|_]).
-
-ring(X):-
-	name(X,[114|_]).
-
-ringlist(X):- 
-	name(X,[114|_]).
-
-% ringlist([]).
-% ringlist([Ring|Rings]):-
-        % ring(Ring),
-        % ringlist(Ringlist).
-
-
-
-% element(br).
-% element(c).
-% element(cl).
-% element(f).
-% element(h).
-% element(i).
-% element(n).
-% element(o).
-% element(s).
-% 
-% 
-% background knowledge 
-
-gteq(X,Y):-
-	not(var(X)), not(var(Y)),
-	float(X), float(Y), 
-	X >= Y, !.
-gteq(X,X):-
-	not(var(X)),
-	float(X).
-
-lteq(X,Y):-
-	not(var(X)), not(var(Y)),
-	float(X), float(Y),
-	X =< Y, !.
-lteq(X,X):-
-	not(var(X)), 
-	float(X).
-
-
-% :- set(i,2).
-% :- set(nodes,20000).
-% :- set(noise,5).
-% :- set(c,3).
-% :- set(verbose,0).
-% :- noreductive.
-% :- nosplit.
--- learningtasks/mutagenesis/prolog/data/aleph/ring_structure.pl
+++
@@ -1,8 +0,0 @@
-/* Prolog theory for nitro aromatic and heteroaromatic compounds
-It expects the Quintus libraries. Quanta bond type 7 is aromatic. */
-
-% :- [library(basics)].
-% :- [library(sets)].
-
-%****************************************************************************
-
