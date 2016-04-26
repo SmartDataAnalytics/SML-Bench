081d1d55a59253a9158dc702060345f04eb2c9bb
Simon Bin <sbin@informatik.uni-leipzig.de>
2016-04-26 16:28:02 +0200

    add dir constants and up java version

---
+++ src/main/java/org/aksw/mlbenchmark/Constants.java
@@ -0,0 +1,10 @@
+package org.aksw.mlbenchmark;
+
+/**
+ * Created by Simon Bin on 16-4-26.
+ */
+public class Constants {
+	public static final String LEARNINGSYSTEMS = "learningsystems";
+	public static final String LEARNINGTASKS = "learningtasks";
+	public static final String LEARNINGPROBLEMS = "lp";
+}
---
+++ src/main/java/org/aksw/mlbenchmark/process/ProcessRunner.java
@@ -0,0 +1,7 @@
+package org.aksw.mlbenchmark.process;
+
+/**
+ * Created by Simon Bin on 16-4-24.
+ */
+public class ProcessRunner {
+}
