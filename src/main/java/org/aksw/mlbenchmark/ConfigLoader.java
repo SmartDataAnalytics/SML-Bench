8ff453e4de7b6d8d4114b586097b976d4258d6f5
Simon Bin <sbin@informatik.uni-leipzig.de>
2016-04-27 23:58:58 +0200

    config loader code and some tests

--- src/main/java/org/aksw/mlbenchmark/ConfigLoader.java
+++ src/main/java/org/aksw/mlbenchmark/ConfigLoader.java
@@ -1,10 +1,13 @@
 package org.aksw.mlbenchmark;
 
+import org.aksw.mlbenchmark.config.FlatConfigHierarchicalConverter;
 import org.aksw.mlbenchmark.config.INIConfigurationWriteDotkeys;
 import org.aksw.mlbenchmark.config.PropertiesConfigurationFromDotkeys;
 import org.apache.commons.configuration2.*;
+import org.apache.commons.configuration2.builder.BuilderParameters;
 import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
 import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.convert.DefaultListDelimiterHandler;
 import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.commons.configuration2.plist.PropertyListConfiguration;
 import org.apache.commons.configuration2.plist.XMLPropertyListConfiguration;
@@ -14,6 +17,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Loading of config files
@@ -27,17 +32,35 @@ public class ConfigLoader {
 		this.filename = filename;
 	}
 
+	/**
+	 * tries to find a config file with the given prefix and a set of implemented extensions
+	 * @param prefix
+	 * @return
+	 */
+	public static ConfigLoader findConfig(String prefix) {
+		final String[] extns = { ".plist", ".xml", ".ini", ".conf", ".prop", ".properties" };
+		for (String ext: extns) {
+			try {
+				return new ConfigLoader(prefix + ext).load();
+			} catch (ConfigLoaderException e) {
+				// ignore any errors
+			}
+		}
+		return null;
+	}
+
 	public final ConfigLoader load() throws ConfigLoaderException {
 		if (filename.endsWith(".plist")) {
 			config = loadFile(PropertyListConfiguration.class);
 		} else if (filename.endsWith(".xml")) {
 			config = loadFile(XMLPropertyListConfiguration.class);
 		} else if (filename.endsWith(".ini") || filename.endsWith(".conf")) {
-			config = loadINIFile();
+			config = FlatConfigHierarchicalConverter.convert(loadINIFile());
 		} else if (filename.endsWith(".prop") || filename.endsWith(".properties")) {
-			CombinedConfiguration config2 = new CombinedConfiguration();
-			config2.addConfiguration(loadFile(PropertiesConfiguration.class));
-			config = config2;
+			//CombinedConfiguration config2 = new CombinedConfiguration();
+			//config2.addConfiguration(loadFile(PropertiesConfiguration.class));
+			//config = config2;
+			config = FlatConfigHierarchicalConverter.convert(loadFile(PropertiesConfiguration.class));
 		} else {
 			throw new ConfigLoaderException("Loading of config type not implemented yet.");
 		}
@@ -67,13 +90,16 @@ public class ConfigLoader {
 	private <T extends FileBasedConfiguration>
 	T loadFile(Class<T> type) throws ConfigLoaderException {
 		Parameters params = new Parameters();
+		List<BuilderParameters> config = new ArrayList<BuilderParameters>();
+		config.add(params.fileBased().setFileName(filename));
+		config.add(params.basic().setThrowExceptionOnMissing(true));
+		if (INIConfiguration.class.isAssignableFrom(type)
+				|| PropertiesConfiguration.class.isAssignableFrom(type)) {
+			config.add(params.basic().setListDelimiterHandler(new DefaultListDelimiterHandler(',')));
+		}
 		FileBasedConfigurationBuilder<T> builder =
-		    new FileBasedConfigurationBuilder<T>(type)
-		    .configure(
-				    params.fileBased()
-						    .setFileName(filename),
-				    params.basic()
-						    .setThrowExceptionOnMissing(true));
+				new FileBasedConfigurationBuilder<T>(type)
+						.configure(config.toArray(new BuilderParameters[0]));
 
 		try
 		{
@@ -94,4 +120,25 @@ public class ConfigLoader {
 		xml.write(new FileWriter("out.xml"));
 	}
 	*/
+
+	public static void main(String[] args) throws ConfigLoaderException, ConfigurationException, IOException {
+		logger.info("---- testing ini ----");
+		HierarchicalConfiguration<ImmutableNode> conf = new ConfigLoader("test.ini").load().config();
+		INIConfiguration iniConfiguration = new INIConfigurationWriteDotkeys(conf);
+		iniConfiguration.write(new FileWriter("test2.ini"));
+		PropertiesConfiguration propertiesConfiguration = new PropertiesConfigurationFromDotkeys();
+		propertiesConfiguration.copy(conf);
+		propertiesConfiguration.write(new FileWriter("test2.prop"));
+		//new Hierarchicalconfigurationcon
+		Testing testing = new Testing();
+		testing.conf(conf);
+
+		new PropertyListConfiguration(conf).write(new FileWriter("test-from-ini.plist"));
+		logger.info("---- testing plist ----");
+		HierarchicalConfiguration<ImmutableNode> config1 = new ConfigLoader("test.plist").load().config();
+		testing.conf(config1);
+		PropertyListConfiguration plc = new PropertyListConfiguration(config1);
+		plc.write(new FileWriter("test-copy.plist"));
+	}
+
 }
