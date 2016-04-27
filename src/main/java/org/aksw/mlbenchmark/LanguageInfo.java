5d8c74046e7464a1e84fccba87ce2baf14342197
Simon Bin <sbin@informatik.uni-leipzig.de>
2016-04-28 01:57:02 +0200

    language info factory

--- src/main/java/org/aksw/mlbenchmark/LanguageInfo.java
+++ src/main/java/org/aksw/mlbenchmark/LanguageInfo.java
@@ -1,7 +1,23 @@
 package org.aksw.mlbenchmark;
 
+import org.aksw.mlbenchmark.languages.LanguageInfoBase;
+import org.aksw.mlbenchmark.languages.OwlLanguageInfo;
+import org.aksw.mlbenchmark.languages.PrologLanguageInfo;
+import org.apache.commons.lang3.NotImplementedException;
+
 /**
  * Created by Simon Bin on 16-4-28.
  */
 public class LanguageInfo {
+	static LanguageInfoBase forLanguage(String language) {
+		switch (Constants.LANGUAGES.valueOf(language.toUpperCase())) {
+
+			case OWL: return new OwlLanguageInfo();
+
+			case PROLOG: return new PrologLanguageInfo();
+
+			default: throw new NotImplementedException("No Language Info for " + language);
+
+		}
+	}
 }
