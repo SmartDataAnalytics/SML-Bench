cf3299b0cbd902b0a8200b3c6785b54b4dfc47bf
Simon Bin <sbin@informatik.uni-leipzig.de>
2016-04-27 23:58:25 +0200

    continue benchmark runner

--- src/main/java/org/aksw/mlbenchmark/BenchmarkRunner.java
+++ src/main/java/org/aksw/mlbenchmark/BenchmarkRunner.java
@@ -1,6 +1,7 @@
 package org.aksw.mlbenchmark;
 
 import org.aksw.mlbenchmark.process.ProcessRunner;
+import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.HierarchicalConfiguration;
 import org.apache.commons.configuration2.tree.ImmutableNode;
 import org.slf4j.Logger;
@@ -11,8 +12,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Simon Bin on 16-4-13.
@@ -24,6 +24,10 @@ public class BenchmarkRunner {
 	private final String rootDir;
 	private List<String> availableLearningSystems = new LinkedList<>();
 	private final HierarchicalConfiguration<ImmutableNode> config;
+	private final List<String> desiredSystems;
+	private final Set<String> desiredLanguages = new HashSet<>();
+	private final Map<String, String> systemLanguage = new HashMap<>();
+	private Path tempDirectory;
 
 
 	public BenchmarkRunner(HierarchicalConfiguration<ImmutableNode> config) {
@@ -39,6 +43,30 @@ public class BenchmarkRunner {
 		}
 		logger.info("available learning systems:" + availableLearningSystems);
 		this.config = config;
+		desiredSystems = config.getList(String.class, "learningsystems");
+		initLanguages();
+		initTemp();
+	}
+
+	private void initTemp() {
+		try {
+			tempDirectory = Files.createTempDirectory(new File(currentDir).toPath(), "sml-temp");
+			logger.info("using working directory: " + tempDirectory);
+			tempDirectory.toFile().deleteOnExit();
+		} catch (IOException e) {
+			throw new RuntimeException("Cannot create temporary folder, terminating.", e);
+		}
+	}
+
+	private void initLanguages() {
+		for (final String sys: desiredSystems) {
+			LearningSystemInfo learningSystemInfo = new LearningSystemInfo(this, sys);
+			String language = learningSystemInfo.getConfig().getString("language");
+			desiredLanguages.add(language);
+			systemLanguage.put(sys, language);
+			logger.info("language for "+sys+": " +language);
+		}
+
 	}
 
 	public String getLearningSystemDir(String learningSystem) {
@@ -59,18 +87,37 @@ public class BenchmarkRunner {
 		this(new ConfigLoader(configFilename).load().config());
 	}
 
-	public void run() throws IOException {
-		List<String> desiredSystems = config.getList(String.class, "learningsystems");
+	public Configuration getConfig() {
+		return config;
+	}
+
+	public void run() {
 		logger.info("benchmarking systems: " + desiredSystems);
-		Path tempDirectory = Files.createTempDirectory(new File(currentDir).toPath(), "sml-temp");
-		for (String system: desiredSystems) {
+
+		List<String> scenarios = config.getList(String.class, "scenarios");
+
+		logger.info("requested scenarios: " + scenarios);
+		for (final String scn : scenarios) {
+			runScenario(scn);
+		}
+		Map<String, Map<Integer, List<String> >> languageFolds = new HashMap<>();
+		for (final String lang : desiredLanguages) {
+
+			//languageFolds.put(lang, CrossValidation)
+		}
+		for (final String system: desiredSystems) {
 			try {
 				ProcessRunner processRunner = new ProcessRunner(getLearningSystemDir(system), "./run");
 			} catch (IOException e) {
 				logger.warn("learning system "+ system + " could not execute: " + e.getMessage());
 			}
 		}
-		Testing testing = new Testing();
-		testing.conf(config);
+		//Testing testing = new Testing();
+		//testing.conf(config);
+	}
+
+	private void runScenario(String scn) {
+		final String[] split = scn.split("/");
+		
 	}
 }
