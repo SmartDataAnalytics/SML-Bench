5ae190271093774e0e93b49ac802d5f8226c2027
Simon Bin <sbin@informatik.uni-leipzig.de>
2016-04-28 05:48:52 +0200

    process runner

--- src/main/java/org/aksw/mlbenchmark/LearningSystemInfo.java
+++ src/main/java/org/aksw/mlbenchmark/LearningSystemInfo.java
@@ -11,14 +11,11 @@ import org.apache.commons.configuration2.tree.MergeCombiner;
 public class LearningSystemInfo {
 	final String learningSystem;
 	final BenchmarkRunner br;
+	private Configuration config;
+
 	public LearningSystemInfo(BenchmarkRunner parent, String learningSystem) {
 		this.learningSystem = learningSystem;
 		this.br = parent;
-	}
-	public String getDir() {
-		return br.getLearningSystemDir(learningSystem);
-	}
-	public Configuration getConfig() {
 		Configuration defaultConfig = new BaseConfiguration();
 		defaultConfig.setProperty("language", learningSystem.toLowerCase().equals("dllearner") ? "owl" : "prolog");
 		Configuration runtimeConfig = br.getConfig();
@@ -26,11 +23,33 @@ public class LearningSystemInfo {
 		ConfigLoader systemCL = ConfigLoader.findConfig(getDir()+"/"+"system");
 		CombinedConfiguration cc = new CombinedConfiguration();
 		cc.setNodeCombiner(new MergeCombiner());
+		cc.addConfiguration(lsRuntimeConfig);
 		if (systemCL != null) {
 			cc.addConfiguration(systemCL.config());
 		}
-		cc.addConfiguration(lsRuntimeConfig);
 		cc.addConfiguration(defaultConfig);
-		return cc;
+		defaultConfig.setProperty("configFormat", "owl".equals(cc.getString("language")) ? "prop" : "conf");
+		config = cc;
+	}
+	public String getDir() {
+		return br.getLearningSystemDir(learningSystem);
+	}
+	public Configuration getConfig() {
+		return config;
+	}
+
+	public String getPosFilename() {
+		return config.getString("filename.pos",
+				LanguageInfo.forLanguage(config.getString("language")).getPosFilename());
+	}
+
+	public String getNegFilename() {
+		return config.getString("filename.neg",
+				LanguageInfo.forLanguage(config.getString("language")).getNegFilename());
+	}
+
+	public String getBaseFilename() {
+		return config.getString("filename.base",
+				LanguageInfo.forLanguage(config.getString("language")).getBaseFilename());
 	}
 }
--- src/main/java/org/aksw/mlbenchmark/process/ProcessRunner.java
+++ src/main/java/org/aksw/mlbenchmark/process/ProcessRunner.java
@@ -1,5 +1,6 @@
 package org.aksw.mlbenchmark.process;
 
+import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
 import org.apache.commons.exec.PumpStreamHandler;
@@ -10,7 +11,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
  * This class will run a shell script
@@ -24,23 +28,51 @@ public class ProcessRunner {
 
 	}*/
 
-	public ProcessRunner(String learningSystemDir, String command, List<String> args) throws IOException {
+	public ProcessRunner(String learningSystemDir, String command, List<String> args, Configuration configuration) throws IOException {
 		DefaultExecutor e = new DefaultExecutor();
-		try {
-			e.setStreamHandler(new PumpStreamHandler(new FileOutputStream("lsoutput.log")));
-		} catch (FileNotFoundException e1) {
-			logger.warn("could not set log file output");
-		}
-		e.setWorkingDirectory(new File(learningSystemDir));
 
+		e.setWorkingDirectory(new File(learningSystemDir));
+		Map<String, String> environment = new ProcessBuilder().environment();
+		if (configuration != null) {
+			try {
+				e.setStreamHandler(new PumpStreamHandler(new FileOutputStream(configuration.getString("data.workdir")+"/"+"lsoutput.log")));
+			} catch (FileNotFoundException e1) {
+				logger.warn("could not set log file output");
+			}
+			updateEnvironment(environment, configuration);
+		}
 		CommandLine cmd = new CommandLine("./run");
 		if (args != null) {
 			cmd.addArguments(args.toArray(new String[0]));
 		}
-		int rc = e.execute(cmd);
+		int rc = e.execute(cmd, environment);
+	}
+
+	private void updateEnvironment(Map<String, String> environment, Configuration configuration) {
+		Iterator<String> keys = configuration.getKeys();
+
+		while (keys.hasNext()) {
+			String key = keys.next();
+			Object property = configuration.getProperty(key);
+			String val;
+			if (property instanceof Collection) {
+				StringBuilder valBuilder = new StringBuilder();
+				Iterator iterator = ((Collection) property).iterator();
+				while (iterator.hasNext()) {
+					valBuilder.append(iterator.next());
+					if (iterator.hasNext()) {
+						valBuilder.append(":");
+					}
+				}
+				val = valBuilder.toString();
+			} else {
+				val = property.toString();
+			}
+			environment.put("SMLB_" + key.toUpperCase().replaceAll("[.]+", "_"), val);
+		}
 	}
 
 	public ProcessRunner(String learningSystemDir, String s) throws IOException {
-		this(learningSystemDir, s, null);
+		this(learningSystemDir, s, null, null);
 	}
 }
