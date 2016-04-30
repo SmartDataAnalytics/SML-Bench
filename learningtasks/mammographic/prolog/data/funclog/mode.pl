a6eb60dbe929840e9e240e81a29adf60745c2844
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2016-04-30 13:53:31 +0200

    added Mammographic dataset and lp

---
+++ learningtasks/mammographic/prolog/data/funclog/mode.pl
@@ -0,0 +1,26 @@
+:- modeh(1,ex(+patient)).
+:- modeb(1,'Density'(+measure)).
+:- modeb(1,'Margin'(+measure)).
+:- modeb(1,'Shape'(+measure)).
+:- modeb(1,circumscribed(+measure)).
+:- modeb(1,'fat-containing'(+measure)).
+:- modeb(1,high(+measure)).
+:- modeb(1,'ill-defined'(+measure)).
+:- modeb(1,irregular(+measure)).
+:- modeb(1,iso(+measure)).
+:- modeb(1,lobular(+measure)).
+:- modeb(1,low(+measure)).
+:- modeb(1,microlobulated(+measure)).
+:- modeb(1,obscured(+measure)).
+:- modeb(1,oval(+measure)).
+:- modeb(1,round(+measure)).
+:- modeb(1,spiculated(+measure)).
+:- modeb(1,unknown(+measure)).
+:- modeb(1,hasAge(+patient,#double)).
+:- modeb(1,hasBiRads(+patient,#double)).
+:- modeb(1,hasDensity(+patient,-measure)).
+:- modeb(1,hasMargin(+patient,-measure)).
+:- modeb(1,hasShape(+patient,-measure)).
+%%% :- modeb(2,hasDensity(+patient,-density)).
+%%% :- modeb(2,hasMargin(+patient,-margin)).
+%%% :- modeb(2,hasShape(+patient,-shape)).
