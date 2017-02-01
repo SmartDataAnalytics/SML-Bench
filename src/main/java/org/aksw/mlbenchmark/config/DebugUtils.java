9d641ca8bbf16875e9256401003fd1ed3fb8f970
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2017-02-01 16:56:55 +0100

    Updated DebugUtils
    - added configToString method

--- src/main/java/org/aksw/mlbenchmark/config/DebugUtils.java
+++ src/main/java/org/aksw/mlbenchmark/config/DebugUtils.java
@@ -3,14 +3,26 @@ package org.aksw.mlbenchmark.config;
 import java.util.Iterator;
 
 import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.lang3.StringUtils;
 
 public class DebugUtils {
 	public static void printConfig(Configuration conf) {
+		System.out.println(configToString(conf));
+	}
+	
+	public static String configToString(Configuration conf) {
+		StringBuilder res = new StringBuilder();
+		
 		Iterator<String> it = conf.getKeys();
 		
 		while (it.hasNext()) {
 			String k = it.next();
-			System.out.println(k + " = " + conf.getString(k));
+			res.append(k);
+			res.append(" = ");
+			String[] v = conf.getStringArray(k);
+			res.append(StringUtils.join(v, ','));
+			res.append('\n');
 		}
+		return res.toString();
 	}
 }
