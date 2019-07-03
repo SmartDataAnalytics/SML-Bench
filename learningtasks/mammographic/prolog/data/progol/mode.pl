cb7967ac01802211ee619502b1c5ea7b4edbe0fb
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2019-07-03 13:22:54 +0200

    Updated mammographic dataset + lps
    Mainly replaced all quoted atoms by real (lowercase) atoms.

--- learningtasks/mammographic/prolog/data/progol/mode.pl
+++ learningtasks/mammographic/prolog/data/progol/mode.pl
@@ -1,11 +1,11 @@
 :- modeh(1,ex(+patient))?
-:- modeb(1,'Density'(+measure))?
-:- modeb(1,'Margin'(+measure))?
-:- modeb(1,'Shape'(+measure))?
+:- modeb(1,density(+measure))?
+:- modeb(1,margin(+measure))?
+:- modeb(1,shape(+measure))?
 :- modeb(1,circumscribed(+measure))?
-:- modeb(1,'fat-containing'(+measure))?
+:- modeb(1,fat_containing(+measure))?
 :- modeb(1,high(+measure))?
-:- modeb(1,'ill-defined'(+measure))?
+:- modeb(1,ill_defined(+measure))?
 :- modeb(1,irregular(+measure))?
 :- modeb(1,iso(+measure))?
 :- modeb(1,lobular(+measure))?
@@ -16,11 +16,11 @@
 :- modeb(1,round(+measure))?
 :- modeb(1,spiculated(+measure))?
 :- modeb(1,unknown(+measure))?
-:- modeb(1,hasAge(+patient,#double))?
-:- modeb(1,hasBiRads(+patient,#double))?
-:- modeb(1,hasDensity(+patient,-measure))?
-:- modeb(1,hasMargin(+patient,-measure))?
-:- modeb(1,hasShape(+patient,-measure))?
-%%% :- modeb(2,hasDensity(+patient,-density))?
-%%% :- modeb(2,hasMargin(+patient,-margin))?
-%%% :- modeb(2,hasShape(+patient,-shape))?
+:- modeb(1,has_age(+patient,#double))?
+:- modeb(1,has_bi_rads(+patient,#double))?
+:- modeb(1,has_density(+patient,-measure))?
+:- modeb(1,has_margin(+patient,-measure))?
+:- modeb(1,has_shape(+patient,-measure))?
+%%% :- modeb(2,has_density(+patient,-density))?
+%%% :- modeb(2,has_margin(+patient,-margin))?
+%%% :- modeb(2,has_shape(+patient,-shape))?
