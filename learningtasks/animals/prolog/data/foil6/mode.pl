a33a5dadbdde4c994654fe109d22b2623df6d351
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2019-12-17 16:30:42 +0100

    Added FOIL bias declarations for animals lp

---
+++ learningtasks/animals/prolog/data/foil6/mode.pl
@@ -0,0 +1,13 @@
+target(animal).
+
+type(animal).
+type(has_gills).
+type(homeothermic).
+type(has_eggs).
+type(int, continuous).
+type(covering).
+type(habitat).
+predicate(animal/1,animal).
+predicate(has_covering/2,animal,covering).
+predicate(has_legs/2,animal,int).
+predicate(habitat/2,animal,habitat).
